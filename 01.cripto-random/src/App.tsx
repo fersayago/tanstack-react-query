f009a4afbd2dd4b435295c286dc8fafee37cddc0
fersayago <sayago.fernando@gmail.com>
2022-11-22 13:41:43 -0300

    Numero aleatorio criprografico atmosferico

--- 01.cripto-random/src/App.tsx
+++ 01.cripto-random/src/App.tsx
@@ -1,34 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const getRandomNumberFromApi = async():Promise<number> => {
+  const resp = await fetch('https://www.random.org/integers/?num=1&min=1&max=500&col=1&base=10&format=plain&rnd=new')
+  const numberString: string = await resp.text();
+  return +numberString;
+}
+
+export const App = () => {
+  const [number, setNumber] = useState<number>();
+
+  useEffect(() => {
+    getRandomNumberFromApi()
+      .then( num => setNumber(num) )
+  }, [])
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="App App-header">
+      <h2>Número aleatorio: { number }</h2>
     </div>
   )
 }
-
-export default App
